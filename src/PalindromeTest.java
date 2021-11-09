e578d621c18fbf0ad21625573685ee634740b3f4
Steven Sousa <70921007+Stevensousa67@users.noreply.github.com>
2021-11-09 09:32:19 -0500

    Created JUnit tests for Lab 4

--- src/PalindromeTest.java
+++ src/PalindromeTest.java
@@ -1,16 +1,25 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeTest {
 
     @Test
     void isPalindromeStack() {
-
+        Assertions.assertFalse(false, "ADBA");
+        Assertions.assertFalse(false, "Breski");
+        Assertions.assertTrue(true, "Nurses Run");
+        Assertions.assertTrue(true, "11/11/11");
+        Assertions.assertTrue(true, "ABBA");
+        Assertions.assertTrue(true, "123-45-4321");
     }
 
     @Test
     void isPalindromeQueue() {
-
+        Assertions.assertFalse(false, "ADBA");
+        Assertions.assertFalse(false, "Breski");
+        Assertions.assertTrue(true, "Nurses Run");
+        Assertions.assertTrue(true, "11/11/11");
+        Assertions.assertTrue(true, "ABBA");
+        Assertions.assertTrue(true, "123-45-4321");
     }
 }
